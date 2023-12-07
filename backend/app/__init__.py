bfb040e5cb2601ff7f2f808634ba5fe1cce719e8
Emirhan Böge <64352124+emirhanboge@users.noreply.github.com>
2023-12-07 14:58:41 +0300

    Update __init__.py for CORS

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,20 +3,23 @@ import firebase_admin
 from firebase_admin import credentials, initialize_app
 from dotenv import load_dotenv
 import os
+from flask_cors import CORS
 
 from .controller.user_controller import user_blueprint
 from .controller.artist_controller import artist_blueprint
 from .controller.album_controller import album_blueprint
 from .controller.song_controller import song_blueprint
 
-load_dotenv()  
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-
+    
     if not firebase_admin._apps:
         cred = credentials.Certificate(os.environ.get('FIREBASE_ADMINSDK_JSON_PATH'))
         initialize_app(cred)
+    
+    CORS(app)
 
     from .routes import init_app
 
